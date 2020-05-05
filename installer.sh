97ae498aac217b1077838d0215ecbd1f07a3b90e
Luis Angel Iñiguez Hernández <luigi.lahi@gmail.com>
2020-05-05 14:12:46 -0500

    :bug: Download silently the bin file

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 
 echo "Installing wiggot-sync"
 
-curl 'https://raw.githubusercontent.com/Donluigimx/wiggot-sync/master/wiggot-sync.sh' >/usr/local/bin/wiggot-sync
+curl -s 'https://raw.githubusercontent.com/Donluigimx/wiggot-sync/master/wiggot-sync.sh' >/usr/local/bin/wiggot-sync
 chmod +x /usr/local/bin/wiggot-sync
 
 echo "Installation finished"
